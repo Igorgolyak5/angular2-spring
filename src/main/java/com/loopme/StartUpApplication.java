dc836981bbbb2d6ca47d3c76163996ac8e143ad9
Igor Golyak <igorholyak5@gmail.com>
2019-06-22 15:44:00 +0300

    test

--- src/main/java/com/loopme/StartUpApplication.java
+++ src/main/java/com/loopme/StartUpApplication.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * @author Ihor Holiak
+ */
 @ComponentScan(basePackages = {"com.loopme"})
 @SpringBootApplication
 public class StartUpApplication  {
